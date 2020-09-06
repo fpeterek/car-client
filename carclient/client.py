cb487266f14fc3b95f71b143fbeab3242ea684ab
Filip Peterek <fpeterek@seznam.cz>
2020-09-06 18:25:27 +0200

    Emergency brake

--- carclient/client.py
+++ carclient/client.py
@@ -19,6 +19,7 @@ class _MessageType:
     drive = _to_bytes(0, signed=False)
     healthcheck = _to_bytes(1, signed=False)
     info = _to_bytes(2, signed=False)
+    ebrake = _to_bytes(3, signed=False)
 
 
 def drive(velocity: int, steering_angle: int) -> bool:
@@ -42,16 +43,25 @@ def healthcheck() -> bool:
         return bool(sock.recv(1)[0])
 
 
-def info() -> Tuple[int, int]:
+def info() -> Tuple[int, int, bool]:
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect((_host, _port))
         sock.sendall(_MessageType.info + _padding + _padding)
 
-        data = sock.recv(2)
+        data = sock.recv(3)
 
         v = int.from_bytes(data[0:1], 'little', signed=True)
         s = int.from_bytes(data[1:2], 'little', signed=True)
+        b = bool(int.from_bytes(data[2:3], 'little', signed=True))
 
-        return v, s
+        return v, s, b
 
+
+def ebrake(val: bool) -> bool:
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((_host, _port))
+        sock.sendall(_MessageType.ebrake + _to_bytes(int(val)) + _padding)
+
+        return bool(sock.recv(1)[0])
--- carclient/menu.py
+++ carclient/menu.py
@@ -1,4 +1,4 @@
-from client import drive, healthcheck, info
+from client import drive, healthcheck, info, ebrake
 from car_info import CarInfo
 
 
@@ -10,13 +10,18 @@ def read_s():
     return CarInfo.bound_s(int(input('New steering angle: ')))
 
 
+def read_ebrake():
+    i = input('Emergency brake (1/0): ')
+    return bool(int(i))
+
+
 def menu():
 
     v = 0
     s = 0
 
     while True:
-        print('[a] Set Velocity  [b] Set Steering Angle  [c] Print Current  [d] Healthcheck')
+        print('[a] Set Velocity  [b] Set Steering Angle  [c] Print Current  [d] Healthcheck  [e] Emergency Brake')
         i = input('> ')
         if i == 'a':
             v = read_v()
@@ -26,8 +31,12 @@ def menu():
             drive(v, s)
 
         if i == 'c':
-            cv, cs = info()
+            cv, cs, eb = info()
             print(f'Current (velocity, angle) = ({cv}, {cs})')
 
         if i == 'd':
             print(['Dead', 'Alive'][healthcheck()])
+
+        if i == 'e':
+            ebrake(read_ebrake())
+
