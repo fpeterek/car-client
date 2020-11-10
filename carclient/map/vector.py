251c9f0e21f0f739d2fa9bc9a0318dac8b3494a4
Filip Peterek <fpeterek@seznam.cz>
2020-11-10 20:28:30 +0100

    Added distance to vector

--- carclient/map/vector.py
+++ carclient/map/vector.py
@@ -1,3 +1,5 @@
+from geopy import distance
+
 from map.position import Position
 
 
@@ -5,3 +7,4 @@ class Vector:
     def __init__(self, begin: Position, end: Position):
         self.begin = begin
         self.end = end
+        self.dist = distance.distance(begin.tuple, end.tuple).m
